4ee73e7ca9650176bb256594f53e0ea858aa71a6
stkirk <shaanekirk@gmail.com>
2021-08-13 20:40:49 -0400

    scaffolded useForm to mimic useState and hooked it up to CheckoutForm with successful import and functionality

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,10 @@
+import { useState } from "react";
 // write your custom hook here to control your checkout form
+
+const useForm = (initialValue) => {
+  const [values, setValues] = useState(initialValue);
+
+  return [values, setValues];
+};
+
+export default useForm;
