fd930e061b4936f455d83adbe2bb9d8c89c60190
stkirk <shaanekirk@gmail.com>
2021-08-13 20:26:43 -0400

    added constructor and state to PlantList, used componentDidMount for an axios call for an array of plants object and set that to state property called plants, plants now render

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,16 +3,28 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        
-      });
+    axios
+      .get("http://localhost:3333/plants")
+      .then((res) => {
+        console.log("res.data", res.data);
+        this.setState({
+          ...this.state,
+          plants: res.data,
+        });
+      })
+      .catch((err) => console.log("axios error", err));
   }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
